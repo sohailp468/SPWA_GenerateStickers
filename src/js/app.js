3ce2e3b1a0a7e76cb64efd94b38a0e29a28df912
3kidsinatrenchcoat <3kidsinatrenchcoat@Digitals-Mac-Pro.local>
2019-03-12 11:14:32 +0000

    finished

--- src/js/app.js
+++ src/js/app.js
@@ -3,7 +3,7 @@
     'use strict';
 
     // creates an angular module called  - bound to body through ng-app directive
-    var app = angular.module('anti_park_spwa', []);
+    var app = angular.module('anti_park_spwa', [ 'monospaced.qrcode' ]);
 
     // creating controller
     app.controller('Main', control);
@@ -16,6 +16,7 @@
 
         var vm = angular.extend(this, {
             title: 'Welcome to the Sticker App',
+            stickers: [],
             events: [],
             endpoint: ''
         });
@@ -39,6 +40,33 @@
             $http.get(vm.endpoint + 'incidents/16d5b9d9-77a8-4709-bfc7-a080512af177').then(
                 function success(response) {
                     vm.incident = response.data;
+                    console.info(response);
+                },
+                function failure(err) {
+                    console.error(err);
+                }
+            )
+        };
+
+        vm.postStickers = function () {
+            // Using an unauthenticated API here
+            var x = document.getElementById("form1");
+            var y= "";
+            y = x.elements[0].value;
+            var z = parseInt(y);
+            var jsonpayload={"num_requested":z};
+            document.getElementById("test").innerHTML = z;
+             //Use $http service to send get request to API and execute different functions depending on whether it is successful or not
+            $http.post(vm.endpoint + 'stickers/',JSON.stringify(jsonpayload)).then(
+                function success(response) {
+                    while(vm.stickers.length>0) {
+                        vm.stickers.pop();
+                    }
+                    for (var i in response.data) {
+                        vm.stickers.push(response.data[i]);
+                    }
+                    //vm.stickers = response.data;
+                    console.log("vm.stickers: ",vm.stickers.length)
                     console.info(response);
                 },
                 function failure(err) {
